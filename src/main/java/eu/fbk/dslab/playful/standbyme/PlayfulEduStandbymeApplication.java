8dac518020c1668c88e41315d13b05a7e6e65535
micnori <micnori@users.noreply.github.com>
2023-09-21 11:17:38 +0200

    first import

---
+++ src/main/java/eu/fbk/dslab/playful/standbyme/PlayfulEduStandbymeApplication.java
@@ -0,0 +1,13 @@
+package eu.fbk.dslab.playful.standbyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayfulEduStandbymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayfulEduStandbymeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/fbk/dslab/playful/standbyme/PlayfulEduStandbymeApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.fbk.dslab.playful.standbyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayfulEduStandbymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
